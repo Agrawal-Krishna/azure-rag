bdae9b4d3298d05e78b8a30708f607c3a885f0c5
Krishna7agr <97258046+Krishna7agr@users.noreply.github.com>
2024-08-23 16:44:17 +0530

    Create tempCodeRunnerFile.py

---
+++ webapp/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import os
+import openai
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import AzureSearch
